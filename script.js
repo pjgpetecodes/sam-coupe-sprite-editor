dc02e2e9e4981fb380adc19243416b9d1f582356
Peter Gallagher <pete@pjgcreations.co.uk>
2023-03-06 23:24:41 +0000

    Working on Colour Picker

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const grid = document.querySelector('#grid');
+const colorGrid = document.querySelector('#color-grid');
 const colorPicker = document.querySelector('#color-picker');
 const clearButton = document.querySelector('#clear');
 const convertButton = document.querySelector('#convert');
 
 const colors = ['#ffffff', '#c7c7c7', '#9f9f9f', '#7f7f7f', '#5f5f5f', '#3f3f3f', '#1f1f1f', '#000000', '#ff9c9c', '#ff0000', '#c70000', '#9f0000', '#7f0000', '#5f0000', '#3f0000', '#1f0000'];
 
+// create the color grid
+for (let i = 0; i < colors.length; i++) {
+  const colorCell = document.createElement('div');
+  colorCell.classList.add('color-cell');
+  colorCell.style.backgroundColor = colors[i];
+  if (i === 0) {
+    colorCell.classList.add('selected');
+  }
+  colorCell.addEventListener('click', () => {
+    document.querySelector('.selected').classList.remove('selected');
+    colorCell.classList.add('selected');
+    colorPicker.value = colors[i];
+  });
+  colorGrid.appendChild(colorCell);
+}
+
+// create the main grid
 for (let i = 0; i < 1024; i++) {
   const cell = document.createElement('div');
   cell.classList.add('cell');
-
   cell.addEventListener('click', () => {
     cell.style.backgroundColor = colorPicker.value;
   });
-
   grid.appendChild(cell);
 }
 
+// clear button click event
 clearButton.addEventListener('click', () => {
   const cells = document.querySelectorAll('.cell');
   cells.forEach(cell => {
@@ -23,6 +40,7 @@ clearButton.addEventListener('click', () => {
   });
 });
 
+// convert button click event
 convertButton.addEventListener('click', () => {
   const cells = document.querySelectorAll('.cell');
   const bytes = [];
