ab4813062ef1284ba450aea6243097b32fd79b34
Peter Gallagher <pete@pjgcreations.co.uk>
2023-03-07 00:06:35 +0000

    Working on Convert Function

--- script.js
+++ script.js
@@ -23,13 +23,22 @@ for (let i = 0; i < colors.length; i++) {
 }
 
 // create the main grid
+// create the main grid
 for (let i = 0; i < 1024; i++) {
-  const cell = document.createElement('div');
-  cell.classList.add('cell');
-  cell.addEventListener('click', () => {
-    cell.style.backgroundColor = colorPicker.value;
-  });
-  grid.appendChild(cell);
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.originalColor = '';
+    cell.addEventListener('click', () => {
+        const currentColor = cell.style.backgroundColor;
+        if (hexToRgb(colorPicker.value) === currentColor) {
+          cell.style.backgroundColor = cell.dataset.previousColor;
+        } else {
+          cell.dataset.previousColor = currentColor;
+          cell.style.backgroundColor = colorPicker.value;
+        }
+      });
+      
+    grid.appendChild(cell);
 }
 
 // clear button click event
@@ -44,13 +53,24 @@ clearButton.addEventListener('click', () => {
 
 // convert button click event
 convertButton.addEventListener('click', () => {
-  const cells = document.querySelectorAll('.cell');
-  const bytes = [];
-  for (let i = 0; i < cells.length; i += 2) {
-    const highNibble = colors.indexOf(cells[i].style.backgroundColor).toString(16);
-    const lowNibble = colors.indexOf(cells[i + 1].style.backgroundColor).toString(16);
-    const byte = (highNibble.length === 2 ? highNibble : '0' + highNibble) + (lowNibble.length === 2 ? lowNibble : '0' + lowNibble);
-    bytes.push(byte);
+    const cells = document.querySelectorAll('.cell');
+    let bytes = '';
+    for (let i = 0; i < cells.length; i += 2) {
+      const highNibble = colors.indexOf(cells[i].style.backgroundColor).toString(16);
+      const lowNibble = colors.indexOf(cells[i + 1].style.backgroundColor).toString(16);
+      const byte = (highNibble.length === 2 ? highNibble : '0' + highNibble) + (lowNibble.length === 2 ? lowNibble : '0' + lowNibble);
+      bytes += byte;
+    }
+    const hexArray = bytes.match(/.{1,32}/g);
+    console.log(hexArray);
+  });
+  
+
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+  
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : null;
   }
-  console.log(bytes);
-});
